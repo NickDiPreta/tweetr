cff5964170521372edddba82bab8c8dfe39a95e3
nick <nicholasdipreta2020@gmail.com>
2020-08-03 16:53:53 -0400

    Commit 7: Tweet show

--- tweeter_app_api/app/controllers/tweets_controller.rb
+++ tweeter_app_api/app/controllers/tweets_controller.rb
@@ -6,7 +6,7 @@ class TweetsController < ApplicationController
 
     def show
         puts "show route: params #{params}"
-        tweet = Tweet.find(params[:id])
+        tweet = Tweet.find(params[:id] )
         render json: { status: 200, tweet: tweet }
       end
 
